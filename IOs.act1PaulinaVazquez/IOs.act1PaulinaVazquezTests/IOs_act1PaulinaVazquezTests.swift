f953f6ae08aff045ad2317397c0c36fd03054a92
PaulinaVaz <156823303+PaulinaVaz@users.noreply.github.com>
2026-01-20 19:42:20 -0600

    Add files via upload
    Pre actividad 1

---
+++ IOs.act1PaulinaVazquez/IOs.act1PaulinaVazquezTests/IOs_act1PaulinaVazquezTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOs_act1PaulinaVazquezTests.swift
+//  IOs.act1PaulinaVazquezTests
+//
+//  Created by Alumno on 20/01/26.
+//
+
+import Testing
+@testable import IOs_act1PaulinaVazquez
+
+struct IOs_act1PaulinaVazquezTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
